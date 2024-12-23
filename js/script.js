9b0e1f1011ba67b0f9062b233c28278e9cae7733
Arkadiusz Pedzisz <serwis.artech@gmail.com>
2024-12-23 19:21:05 +0100

    uzupelnienie

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 'use strict';
-let counter = 0;
+'use strict';
 
-caunter = counter + 1;
+function titleClickHandler(event) {
+    const clickedElement = this;
+    console.log('Link was clicked!');
 
-console.log(counter); // 0
+    /* [DONE] remove class 'active' from all article links  */
+
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
+
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    console.log('clickedElement:', clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+}
