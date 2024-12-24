6219ff977ebd33fc6d6af3f1aadc75189ec2cf14
Arkadiusz Pedzisz <serwis.artech@gmail.com>
2024-12-24 02:46:44 +0100

    nowe pliki testujace

--- js/script.js
+++ js/script.js
@@ -1,33 +1,40 @@
 'use strict';
-'use strict';
-
 function titleClickHandler(event) {
-    const clickedElement = this;
-    console.log('Link was clicked!');
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
-    /* [DONE] remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for (let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
-    }
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
 
-    console.log('clickedElement:', clickedElement);
+  /* [DONE] remove class 'active' from all articles */
 
-    /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post.active');
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
 
-    /* get 'href' attribute from the clicked link */
+  /* get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+  const href = clickedElement.getAttribute('href');
 
-    /* add class 'active' to the correct article */
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const selectedArticle = document.querySelector(href);
+  console.log(selectedArticle);
+
+  /* add class 'active' to the correct article */
+  selectedArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 }
