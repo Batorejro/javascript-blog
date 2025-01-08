29cca5b519bb338ce40788e3c5d1ea1d5bfa98b7
Arkadiusz Pedzisz <serwis.artech@gmail.com>
2025-01-08 23:45:09 +0100

    dodana funkcja z this w js, dodano editor config i eslint

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function titleClickHandler(event) {
   const clickedElement = this;
   console.log('Link was clicked!');
@@ -27,7 +28,7 @@ function titleClickHandler(event) {
 
   /* find the correct article using the selector (value of 'href' attribute) */
   const selectedArticle = document.querySelector(href);
-  console.log(selectedArticle);
+  console.log(selectedArticle)
 
   /* add class 'active' to the correct article */
   selectedArticle.classList.add('active');
@@ -37,4 +38,45 @@ const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
+}
+////////////////////////////////////////////////////////////////
+
+
+
+
+function tagClickHandler(event) {
+  const tagClicked = this;
+  console.log("Tag Clicked: " + tagClicked)
+
+
+  const actLinks = document.querySelectorAll('.tags a');
+
+  for (let link of actLinks) {
+    console.log(link);
+  }
+
+  tagClicked.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
+
+  const activeTags = document.querySelectorAll('.post.active');
+  for (let activeLiTag of activeTags) {
+    activeTags.classList.remove('active');
+  }
+
+  /* get 'href' attribute from the clicked link */
+  const hreft = tagClicked.getAttribute('href');
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const selectedTag = document.querySelector(hreft);
+  console.log(selectedTag)
+
+  /* add class 'active' to the correct article */
+  selectedTag.classList.add('active');
+}
+
+const linksTag = document.querySelectorAll('.tags a');
+
+for (let link of linksTag) {
+  link.addEventListener('click', tagClickHandler);
 }
